ffb70549432cdf25df2c5a56b2f061da3f398257
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-01-22 19:43:50 +0300

    add test hello world path

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,11 @@
 from fastapi import APIRouter
 
 
-api_router = APIRouter()
+api_router = APIRouter(
+    tags = ["hello world"],
+)
+
+
+@api_router.get("/")
+def hello_world():
+    return "hello FastAPI!"
