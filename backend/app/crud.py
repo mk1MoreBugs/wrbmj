41b0aeeeb46628764859bfb3f05efb19047b21a5
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-02-09 16:48:28 +0300

    create user model

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,5 +1,16 @@
-import uuid
-from typing import Any
-
 from sqlmodel import Session, select
 
+from app.models import User
+
+
+def create_user(session: Session, user: User) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+def get_user_by_id(session: Session, user_id: int) -> User | None:
+    statement = select(User).where(User.id == user_id)
+    session_user = session.exec(statement).first()
+    return session_user
---
+++ backend/app/models.py
@@ -0,0 +1,8 @@
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(max_length=42)
+    hashed_password: str
+    photo_file: str
