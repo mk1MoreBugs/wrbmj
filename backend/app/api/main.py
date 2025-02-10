05371ec77f25c92b105ea02701f7c06df2998ed6
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-02-10 18:16:58 +0300

    remove 'hello world' endpoint

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter
 
+from app.api.routes import users
 
-api_router = APIRouter(
-    tags = ["hello world"],
-)
+api_router = APIRouter()
 
-
-@api_router.get("/")
-def hello_world():
-    return "hello FastAPI!"
+api_router.include_router(users.router)
