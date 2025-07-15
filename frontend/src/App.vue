32068aa739219a2325dafca2dcce14a7a9708d3e
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-07-15 18:51:14 +0300

    add custom theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1 class="border-mint-500 text-mint-500 hover:border-transparent hover:bg-red-50 hover:text-white">
+  <h1 class="border-mint-500 text-mint-500 hover:border-transparent hover:bg-red-50 hover:text-white font-script font-medium">
     Hello world!
   </h1>
 
