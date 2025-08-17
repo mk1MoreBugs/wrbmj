f0428a92cce77e8e44ceb6cac39c1f0899654ce8
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-08-17 17:58:33 +0300

    add style to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,14 @@ import LoginView from "./views/LoginView.vue"
 </script>
 
 <template>
-  <LoginView> </LoginView>
+    <div
+      class="
+        block bg-base-100 h-screen
+        font-sans text-body text-white
+      "
+    >
+    <LoginView/>
+    </div>
 </template>
 
 <style scoped></style>
