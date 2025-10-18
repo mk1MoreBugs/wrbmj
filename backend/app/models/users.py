140fbaa7cded7484596aa617629266b003cc3d5b
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-10-18 12:42:50 +0300

    fix

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -14,7 +14,14 @@ class UserInDb(BaseUser, table=True):
     hashed_password: str
     photo_file_name: str
 
-    notes: list["NoteInDb"] | None = Relationship(back_populates="user")
+
+    notes: list["NoteInDb"] | None = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={
+            "primaryjoin": "UserInDb.id == NoteInDb.user_id",
+            "foreign_keys": "[NoteInDb.user_id]"
+        }
+    )
 
 class UserUpload(BaseUser):
     plain_password: str
