3328c015e249fc75952fd5cf361d012a60b47fe6
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-10-26 12:51:58 +0300

    Change the frontend for Docker builds

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,20 +6,46 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+//const isProduction = process.env.ENVIRONMENT === 'production'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    port: 8080,
-  },
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  const commonConfig = {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }
+    }
+  }
+
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      ...commonConfig,
+      plugins: [
+        vue(),
+        vueJsx(),
+        vueDevTools(),
+        tailwindcss(),
+      ],
+      server: {
+        port: 8080,
+      },
+    }
+  } else {
+    // command === 'build'
+
+    return {
+      // build specific config
+      ...commonConfig,
+      plugins: [
+        vue(),
+        vueJsx(),
+        tailwindcss(),
+      ],
+      build: {
+        target: 'esnext',
+        sourcemap: false
+      },
+    }
+  }
 })
