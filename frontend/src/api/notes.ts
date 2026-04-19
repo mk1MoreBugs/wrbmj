4f56f0a7401cdbd5cf3a38795659ff6d708327e2
mk1MoreBugs <oleg1202000@gmail.com>
2026-04-19 16:49:03 +0300

    implement NoteList vue AND Add Note Button

--- frontend/src/api/notes.ts
+++ frontend/src/api/notes.ts
@@ -4,8 +4,13 @@ import type {NoteResponse} from "@/models/ApiResponse.ts"
 
 export const notesApi = {
   async getNotes(): Promise<NoteResponse[]> {
-
     const { data } = await http.get<NoteResponse[]>("/notes/")
+    return data
+  },
+
+  async createNote(): Promise<NoteResponse> {
+    const { data } = await http.post<NoteResponse>("/notes/")
     return data
   }
 }
+
