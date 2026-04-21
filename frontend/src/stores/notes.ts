a7c96935540293be7d22b602527b85e95efd1194
mk1MoreBugs <oleg1202000@gmail.com>
2026-04-21 15:52:15 +0300

    Add additional files in from prev. commit 2

--- frontend/src/stores/notes.ts
+++ frontend/src/stores/notes.ts
@@ -1,4 +1,6 @@
 import { defineStore } from "pinia"
+import {ref} from "vue"
+
 import axios from "axios"
 
 import { notesApi } from "@/api/notes"
@@ -8,29 +10,52 @@ import type { NoteResponse } from "@/models/ApiResponse"
 
 export const useNotesStore = defineStore("notes", () => {
 
-  async function getNotes(): Promise<NoteShortInfo[]> {
-      try {
-        const response: NoteResponse[] = await notesApi.getNotes()
-        const notes: NoteShortInfo[] = response.map((it) => ({
-          id: it.id,
-          lastUpdate: new Date(it.last_update),
-          titleName: it.title_name,
-          shortDescription: it.short_description,
-        }))
-        return notes
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          console.log("Error status:", error.response?.status)
-          console.log("Error data:", error.response?.data)
+  const notes = ref<NoteShortInfo[]>()
 
-        } else {
-          console.error("Non-axios error:", error)
-        }
-        throw error
+  async function fetchNotes(): Promise<void> {
+    try {
+      const response: NoteResponse[] = await notesApi.getNotes()
+      notes.value = response.map((it) => ({
+        id: it.id,
+        lastUpdate: new Date(it.last_update),
+        titleName: it.title_name,
+        shortDescription: it.short_description,
+      }))
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.log("Error status:", error.response?.status)
+        console.log("Error data:", error.response?.data)
+
+      } else {
+        console.error("Non-axios error:", error)
       }
+      throw error
     }
+  }
 
-     return {
-      getNotes
-     }
+  async function createNote(): Promise<NoteShortInfo> {
+    try {
+      const response: NoteResponse = await notesApi.createNote()
+      return {
+        id: response.id,
+        lastUpdate: new Date(response.last_update),
+        titleName: response.title_name,
+        shortDescription: response.short_description,
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.log("Error status:", error.response?.status)
+        console.log("Error data:", error.response?.data)
+      } else {
+        console.error("Non-axios error:", error)
+      }
+      throw error
+    }
+  }
+
+  return {
+    notes,
+    fetchNotes,
+    createNote,
+  }
 })
